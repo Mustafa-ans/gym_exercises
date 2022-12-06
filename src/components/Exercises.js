66d1c6c82c62889014bf0eccd7e5b781b3b32de3
Mustafa-ans <112391427+Mustafa-ans@users.noreply.github.com>
2022-12-06 14:00:07 +0530

    win_scroll

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -5,7 +5,7 @@ import ExerciseCard from './ExerciseCard'
 import {exerciseOptions, fetchData} from '../utils/fetchData'
 
 const Exercises = ({exercises, setExercises, bodyPart}) => {
-  console.log(exercises);
+  // console.log(exercises);
   
   return (
     <Box id="exercises"
@@ -19,7 +19,8 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
     <Stack direction="row" sx={{gap:{lg:'110px', xs:'50px'}}}
     flexwrap="wrap" justifyContent="center">
     {exercises.map((exercise, index)=>{
-      <ExerciseCard key={index} exercise={exercise}/>
+      // <ExerciseCard key={index} exercise={exercise}/>
+      <p>{exercise.name}</p>
     })} 
     
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,9 @@ import Exercises from '../components/Exercises'
 import HeroBanner from '../components/HeroBanner'
 import SearchExercises from '../components/SearchExercises'
 const Home = () => {
-  const [exercises,setExercises] = useState([])
+  
   const [bodyPart, setBodyPart] = useState('all')
+  const [exercises,setExercises] = useState([])
   
   return (
     <Box>
